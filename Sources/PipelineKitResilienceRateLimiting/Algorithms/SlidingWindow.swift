b45f6588595c3e72c0f7f48647170c0ebc73ea8e
gifton <giftono@gmail.com>
2025-09-10 15:28:52 -0700

    linux support autoreleasepool

--- Sources/PipelineKitResilienceRateLimiting/Algorithms/SlidingWindow.swift
+++ Sources/PipelineKitResilienceRateLimiting/Algorithms/SlidingWindow.swift
@@ -1,4 +1,8 @@
 import Foundation
+#if !canImport(Darwin)
+@inline(__always) func autoreleasepool<T>(invoking body: () -> T) -> T { body() }
+@inline(__always) func autoreleasepool<T>(_ body: () -> T) -> T { body() }
+#endif
 
 /// Sliding window implementation for rate limiting.
 internal actor SlidingWindow {
